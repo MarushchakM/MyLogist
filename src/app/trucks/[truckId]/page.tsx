807cddf88d45b5ebcd13eed81777b2560d3e6d01
marushchak <marushchak099@gmail.com>
2025-06-14 22:10:23 +0300

    worck with page truck

--- src/app/trucks/[truckId]/page.tsx
+++ src/app/trucks/[truckId]/page.tsx
@@ -1,47 +1,58 @@
-import { Button } from "@/app/components/Button";
-import { Container } from "@/app/components/Container";
-import { Heading } from "@/app/components/Heading";
+// import { Button } from "@/app/components/Button";
+// import { Container } from "@/app/components/Container";
+// import { Heading } from "@/app/components/Heading";
 import { getTruck } from "@/app/features/trucks/queries/getTruck";
-import { trucksPath } from "@/paths";
-import styles from './TruckPage.module.scss';
-import { LucideTrash } from "lucide-react";
-import { deleteTruck } from "../../features/trucks/actions/deleteTruck";
-import { notFound } from "next/navigation";
+// import { trucksPath } from "@/paths";
+// import styles from './TruckPage.module.scss';
+// import { LucideTrash } from "lucide-react";
+// import { deleteTruck } from "../../features/trucks/actions/deleteTruck";
+// import { notFound } from "next/navigation";
 
-type TicketPageProps = {
-  params: {
-    truckId: string;
-  }
-}
+// type TruckPageProps = {
+//   params: {
+//     truckId: string;
+//   };
+// };
 
-const TruckPage = async ({params}: TicketPageProps) => {
-  const truck = await getTruck(params.truckId);
+const TruckPage = async ({
+  params,
+}: {
+  params: Promise<{ slug: string }>
+  }) => {
   
-  if (!truck) {
-    notFound();
-  }
+  const { slug } = await params
+  const truck = await getTruck(slug)
   
-  return (
-    <>
-      <Container>
-        <Heading title={`Тягач ${truck?.number}`} description="Детальна інформація" />
-        <div className={styles.buttons}>
-          <Button href={trucksPath()} variant="secondary">
-            До списку авто
-          </Button>
-          {truck && (
-            <form action={deleteTruck.bind(null, truck.id)}>
-              <Button variant="danger" type="submit">
-                <LucideTrash />
-              </Button>
-            </form>
-          )}
+  console.log(truck);
+  // const truck = await getTruck(params.truckId);
+  
+  
+  // console.log(truck);
+  // if (!truck) {
+  //   notFound();
+  // }
+  
+  // return (
+  //   <>
+  //     <Container>
+  //       <Heading title={`Тягач ${truck?.number}`} description="Детальна інформація" />
+  //       <div className={styles.buttons}>
+  //         <Button href={trucksPath()} variant="secondary">
+  //           До списку авто
+  //         </Button>
+  //         {truck && (
+  //           <form action={deleteTruck.bind(null, truck.id)}>
+  //             <Button variant="danger" type="submit">
+  //               <LucideTrash />
+  //             </Button>
+  //           </form>
+  //         )}
           
-        </div>
+  //       </div>
         
-      </Container>
-    </>
-  );
+  //     </Container>
+  //   </>
+  // );
 }
 
 export default TruckPage;
