db75b5af6179cd763abc3062f1f9c507c2848f4c
marushchak <marushchak099@gmail.com>
2025-06-15 21:15:19 +0300

    fix problem with build

--- src/app/trucks/page.tsx
+++ src/app/trucks/page.tsx
@@ -1,9 +1,5 @@
-import { Button } from "@/components/Button";
 import { Container } from "@/components/Container";
-import { Heading } from "@/components/Heading";
 import { TruckTable } from "@/features/trucks/components/TruckTable";
-import style from "./TrucksPage.module.scss";
-import { Modal } from "@/components/Modal";
 import { TruckHeading } from "@/features/trucks/components/TruckHeading";
 
 const TrackPage = async () => {
